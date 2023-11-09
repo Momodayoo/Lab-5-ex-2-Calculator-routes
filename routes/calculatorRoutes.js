01666cb743a5060b4932c5e6e2acfd6e1923a186
Momodayoo <msyauqi94@gmail.com>
2023-11-09 20:57:45 +0800

    commit

---
+++ routes/calculatorRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/add', (req, res) => {
+    console.log (req.query)
+    res.send(req.query)
+    })
+
+    
+module.exports = router;
